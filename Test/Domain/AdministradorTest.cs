1d538a8ddd8a47ad028c1591f05ac70dda1650be
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-09-21 11:43:03 -0300

    add FIrst Administrador Get/Set Test done

---
+++ Test/Domain/AdministradorTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MinimalAPI.Dominio.Entidades;
+
+namespace Test.Domain
+{
+[TestClass]
+    public class AdministradorTest
+    {
+        [TestMethod]
+        public void TestarGetSetPropriedades()
+        {
+            //Arrange
+            var administrador = new Administrador();
+
+            //Act
+            administrador.Id = 1;
+            administrador.Email = "test@example.com";
+            administrador.Senha = "teste";
+            administrador.Perfil = "Adm";
+
+            //Assert
+            Assert.AreEqual(1,administrador.Id);
+            Assert.AreEqual("test@example.com",administrador.Email);
+            Assert.AreEqual("teste",administrador.Senha);
+            Assert.AreEqual("Adm",administrador.Perfil);
+        }
+    }
+}
